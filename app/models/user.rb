2838f1942bdda3fe0fc01467d6827b51976b921f
hookabe <hookabe@gmail.com>
2021-05-22 17:11:22 +0900

    コードレビュー後の修正3回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-
   with_options presence: true do
+    validates :nickname
     validates :birthday
 
     with_options format:{ with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/} do
@@ -15,13 +14,13 @@ class User < ApplicationRecord
     end
 
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/} do
-      validates :last_name, presence: true
-      validates :first_name, presence: true
+      validates :last_name
+      validates :first_name
     end
 
     with_options format: { with: /\A[ァ-ヶー－]+\z/} do
-      validates :last_name_kana, presence: true
-      validates :first_name_kana, presence: true
+      validates :last_name_kana
+      validates :first_name_kana
     end
   end
 end
