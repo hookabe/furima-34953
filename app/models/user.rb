85bd9823e9c872465064db5af03778f2f0fccbd2
hookabe <hookabe@gmail.com>
2021-05-22 14:18:20 +0900

    ユーザーモデルでの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
+  validates :encrypted_password, format:{ with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :password, format:{ with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :password_confirmation, format:{ with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
