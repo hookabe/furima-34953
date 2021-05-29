42636c6f9751df5afda57e9cd51d5330b0018922
hookabe <hookabe@gmail.com>
2021-05-29 11:10:40 +0900

    productモデル単体テストコードの記述

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :product do
-    
+    name {Faker::Name.name}
+    text {Faker::Lorem.sentence}
+    category_id {Faker::Number.within(range: 2..11)}
+    status_id {Faker::Number.within(range: 2..7)}
+    delivery_charge_id {Faker::Number.within(range: 2..3)}
+    area_id {Faker::Number.within(range: 2..48)}
+    shipping_date_id {Faker::Number.within(range: 2..4)}
+    price {Faker::Number.within(range: 300..9999999)}
+    association :user 
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
