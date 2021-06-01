7c26d215759b3b8c77de86a8eebf7d00096c1751
hookabe <hookabe@gmail.com>
2021-06-01 21:03:10 +0900

    editアクションの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,10 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
