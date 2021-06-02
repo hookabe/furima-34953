aa3607b9c806ca21b633e550288172c7bef044ab
hookabe <hookabe@gmail.com>
2021-06-02 21:29:16 +0900

    コードレビュー後の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,19 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :move_index, only: [:edit, :update], unless: proc { current_user.id == @product.user_id }
 
   def index
     @products = Product.all.order(created_at: 'DESC')
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
-    unless user_signed_in? && current_user.id == @product.user_id
-      redirect_to action: :index
-    end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path(@product.id)
     else
@@ -39,10 +34,21 @@ class ProductsController < ApplicationController
     end
   end
 
+  
+
   private
 
   def product_params
     params.require(:product).permit(:name, :text, :category_id, :status_id, :delivery_charge_id, :area_id, :shipping_date_id,
                                     :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def move_index
+    redirect_to action: :index
+  end
+
 end
