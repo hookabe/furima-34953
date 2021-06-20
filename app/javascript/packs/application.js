bcb2b1f54bf4c5b9b89a0a3697a349ac92728837
hookabe <hookabe@gmail.com>
2021-06-20 17:04:09 +0900

    画像プレビュー機能の設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../card")
 require("../tax");
+require("../preview")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
